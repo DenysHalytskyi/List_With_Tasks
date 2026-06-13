411f2b9e8bc2336842b7d3a76d0d0ae3db65c1c1
Denys Halytskyi <denys.halytskyi17@gmail.com>
2026-06-13 17:57:43 +0300

    add tag_list page

--- list_with_tasks/urls.py
+++ list_with_tasks/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from list_with_tasks.views import (
     TaskListView,
+    TagListView,
 )
 
 
 app_name = 'list_with_tasks'
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
 ]
